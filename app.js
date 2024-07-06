d54a6a3c5e75abaa7539a6db0fe7b97343229419
joanne-ndirangu <joannendirangu15@gmail.com>
2024-07-07 02:48:31 +0300

    Added search function

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+const searchInput = document.getElementById('search-input');
 const pokedex = document.getElementById("pokedex");
 console.log(pokedex);
+
+let pokemon = [];
+let filteredPokemon = [];
 
 const fetchPokemon = () => {
     const promises = [];
@@ -8,8 +13,9 @@ const fetchPokemon = () => {
         promises.push(fetch(url).then(res => res.json()));
     }
 
-    Promise.all(promises).then(results => {
-        const pokemon = results.map( data => ({
+    Promise.all(promises)
+    .then(results => {
+            pokemon = results.map( data => ({
             name: data.name,
             id: data.id,
             image: data.sprites['front_default'],
@@ -19,24 +25,49 @@ const fetchPokemon = () => {
         }))
         displayPokemon(pokemon);
     })
+    .catch(error => console.error('Error fetching Pokemon:', error));
 };
 
 const displayPokemon = (pokemon) => {
     console.log(pokemon)
-    const pokemonHTMLString = pokemon.map (pokeman =>`
-        <li> 
-            <img src="${pokeman.image}"/>
-            <h2>${pokeman.id}. ${pokeman.name}</h2>
-            <p>Type: ${pokeman.type}</p>
-            <p>Abilities: ${pokeman.abilities.map(ability => ability.ability.name)}</p>
+    const pokemonHTMLString = pokemon.map (pokemon =>`
+        <div class="card"> 
+            <img src="${pokemon.image}"/>
+            <h2>${pokemon.id}. ${pokemon.name}</h2>
+            <p>Type: ${pokemon.type}</p>
+            <p>Abilities: ${pokemon.abilities.map(ability => ability.ability.name)}</p>
             <p>Base Stats:</p>
                     <ul>
-                        ${pokeman.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`)}
+                        ${pokemon.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`)}
                     </ul>
-        </li>
+        </div>
         `)
         .join('');
         pokedex.innerHTML = pokemonHTMLString;
 }
 
+// Function to filter Pokemon based on search term
+const filterPokemon = (searchTerm) => {
+    searchTerm = searchTerm.trim().toLowerCase();
+
+    if (searchTerm === '') {
+        // If search term is empty, reset filteredPokemon to allPokemon
+        filteredPokemon = pokemon.slice();
+    } else {
+        // Filter allPokemon based on search term
+        filteredPokemon = pokemon.filter(pokemon =>
+            pokemon.name.toLowerCase().includes(searchTerm) ||
+            pokemon.id.toString().includes(searchTerm)
+        );
+    }
+    displayPokemon(filteredPokemon); // Display the filtered or all characters
+    console.log(pokemon)
+}
+
+// Search functionality
+searchInput.addEventListener('input', function() {
+    filterPokemon(this.value);
+});
+
 fetchPokemon();
+})
